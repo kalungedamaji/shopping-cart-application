a2b29ade0c769aa219ada1ef7d2a6b1c140d9fdf
Yash <yashsethiya58@gmail.com>
2022-01-13 14:43:47 +0530

    Added testcase for max method.

--- src/main/java/com/technogise/interns/calculator/Calculator.java
+++ src/main/java/com/technogise/interns/calculator/Calculator.java
@@ -5,24 +5,19 @@ public class Calculator {
     public int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
     }
-
     public int minus(int a, int b) {
         return a - b;
     }
-
     public int divide(int a, int b) {
         return a / b;
     }
-
     public int multiply(int a, int b) {
         return a * b;
     }
-
     public int min(int a, int b) {
         return a<b?a:b;
     }
-
-    public int max(int a, int b) {
-        return a>b?a:b;
+    public int max(int firstNumber, int secondNumber) {
+        return firstNumber >= secondNumber ? firstNumber : secondNumber;
     }
 }
--- src/test/java/com/technogise/interns/calculator/CalculatorTest.java
+++ src/test/java/com/technogise/interns/calculator/CalculatorTest.java
@@ -8,11 +8,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    public void testTwoPlusTwoEqualFour() {
+    public void testTwoPlusTwoEqualsFour() {
         int FIRST_NUMBER = 2, SECOND_NUMBER = 2, EXPECTED_SUM = 4;
         Calculator calculator = new Calculator();
         int actualSum = calculator.sum(FIRST_NUMBER,SECOND_NUMBER);
         Assertions.assertEquals(EXPECTED_SUM,actualSum);
+    }
+
+    @Test
+    public void testMaxOfFourAndThreeEqualsFour() {
+        int FIRST_NUMBER = 4, SECOND_NUMBER = 3, EXPECTED_MAX = 4;
+        Calculator calculator = new Calculator();
+        int actualMax = calculator.max(FIRST_NUMBER,SECOND_NUMBER);
+        Assertions.assertEquals(EXPECTED_MAX,actualMax);
     }
 
 }
