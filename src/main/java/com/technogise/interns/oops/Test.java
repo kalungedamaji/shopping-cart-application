6ac39f03a4e13a303f8503e921a1188cb11e1fea
damaji <kalungedamaji1137@gmail.com>
2022-01-13 15:16:32 +0530

    added opps concept code of shopping cart.

--- src/main/java/com/technogise/interns/oops/Test.java
+++
@@ -1,8 +0,0 @@
-package com.technogise.interns.oops;
-
-public class Test {
-    public static void main(String[] args) {
-        int result = (2 + 2) * 4;
-        System.out.println(result);
-    }
-}
