0357f2bb97b330f266c740138a7b646a337968ee
Yash <yashsethiya58@gmail.com>
2022-01-14 12:46:22 +0530

    Added testcase for ShoppingCart Class.

--- src/test/java/com/technogise/interns/oops/ShoppingCartTest.java
+++ src/test/java/com/technogise/interns/oops/ShoppingCartTest.java
@@ -3,12 +3,46 @@ package com.technogise.interns.oops;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ShoppingCartTest {
+    ShoppingCart shoppingCart=new ShoppingCart();
+    Product product=new Product();
 
     @Test
-    public void testSetNumberOfProduct(){
-
+    public void testSetGetNumberOfProduct(){
+        int EXPECTED_NUMEBER_OF_PRODUCTS=5,INPUT_NUMBER_OF_PRODUCTS=5;
+        shoppingCart.setNumberOfProducts(INPUT_NUMBER_OF_PRODUCTS);
+        int actualNumberOfProducts=shoppingCart.getNumberOfProducts();
+        assertEquals(EXPECTED_NUMEBER_OF_PRODUCTS,actualNumberOfProducts);
     }
+    @Test
+    public void testSetProductGetProduct(){
+        Product EXPECTED_PRODUCT=product;
+        shoppingCart.setProduct(product);
+        Product actualProduct=shoppingCart.getProduct();
+        assertEquals(EXPECTED_PRODUCT,actualProduct);
+    }
+    @Test
+    public void testAddProducts(){
+        int EXPECTED_PRODUCTS=5;
+        shoppingCart.addProducts(product,5);
+        int actualProduct=shoppingCart.getNumberOfProducts();
+        assertEquals(EXPECTED_PRODUCTS,actualProduct);
+    }
+    @Test
+    public void testCalculateTotalPrice(){
+        BigDecimal EXPECTED_TOTAL_PRICE=BigDecimal.valueOf(199.95);
+        product.setPrice(BigDecimal.valueOf(39.99));
+        product.setName("Dove");
+        shoppingCart.setProduct(product);
+        shoppingCart.setNumberOfProducts(5);
+        BigDecimal actualTotalPrice=shoppingCart.calculateTotalPrice();
+        assertEquals(EXPECTED_TOTAL_PRICE,actualTotalPrice);
+    }
+
 
 
 
