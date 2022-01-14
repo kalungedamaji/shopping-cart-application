c38f9aff2173db75c8b9344d606ce3f9afc69bd1
damaji <kalungedamaji1137@gmail.com>
2022-01-14 13:24:15 +0530

    added code to break the test case.

--- src/main/java/com/technogise/interns/oops/ShoppingCart.java
+++ src/main/java/com/technogise/interns/oops/ShoppingCart.java
@@ -8,6 +8,7 @@ public class ShoppingCart {
     private static final int TWO_DIGIT_PRECISION =2;
 
     public void addProducts(Product product, int numberOfProducts) {
+        product.setName("XYZ");
        setProduct(product);
        setNumberOfProducts(numberOfProducts);
     }
