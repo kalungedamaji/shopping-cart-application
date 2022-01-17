d43358acc97b1a631c92c7b8fd8f174904036cf1
damaji <kalungedamaji1137@gmail.com>
2022-01-17 17:27:33 +0530

    Modify code for LSP.

--- src/main/java/com/technogise/interns/liskovsubstitutionprinciple/SavingAccount.java
+++ src/main/java/com/technogise/interns/liskovsubstitutionprinciple/SavingAccount.java
@@ -2,9 +2,9 @@ package com.technogise.interns.liskovsubstitutionprinciple;
 
 public class SavingAccount {
     public void deposit(Money  money){
-        System.out.println(" Saving Deposited to the Account");
+        System.out.println("  Deposited to the Saving Account");
     }
     public void withdraw(Money  money){
-        System.out.println("Saving withdrawn From Account ");
+        System.out.println(" withdrawn From Saving Account ");
     }
 }
