0d5f6b375c1054cd720fd4010bf7de6683d1455c
damaji <kalungedamaji1137@gmail.com>
2022-01-18 15:55:23 +0530

    code for DI.

---
+++ src/main/java/com/technogise/interns/dependencyinversion/CustomerDao.java
@@ -0,0 +1,21 @@
+package com.technogise.interns.dependencyinversion;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerDao {
+    public Optional<Customer> findById(int id) {
+        /**
+         *  select * from customer where id=id;
+         */
+        return Optional.empty();
+    }
+
+    public List<Customer> findAll() {
+        /**
+         *  select * from customer;
+         */
+        return Collections.EMPTY_LIST;
+    }
+}
