ac6eee18c13486e847baa097d4d0830a511db632
shrishti-27 <shrishtijain1345@gmail.com>
2022-03-17 12:45:09 +0530

    Added end point for individual product in ProductController

--- src/main/java/com/technogise/interns/shoppingcart/cart/CartController.java
+++ src/main/java/com/technogise/interns/shoppingcart/cart/CartController.java
@@ -1,8 +1,8 @@
 package com.technogise.interns.shoppingcart.cart;
 
-import com.technogise.interns.oops.CartItem;
+import com.technogise.interns.oops.Product;
+import com.technogise.interns.shoppingcart.dto.CartItem;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,18 +13,24 @@ import java.util.List;
 
 @RestController
 public class CartController {
+    @GetMapping("/cart")
+    public ResponseEntity<List<Product>> getAllCartItems(){
+        List<CartItem> cartList = new ArrayList();
 
-    @GetMapping(value="/cart" ,produces= MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<CartItem>> getAllProducts() {
+        CartItem cartItem = new CartItem();
+        cartItem.setName("Laptop");
+        cartItem.setPrice(BigDecimal.TEN);
+        cartItem.setImage("Laptop Img");
+        cartItem.setQuantity(2);
+        cartList.add(cartItem);
 
-        List<CartItem> productList = new ArrayList();
+        CartItem cartItem1 = new CartItem();
+        cartItem1.setName("Laptop");
+        cartItem1.setPrice(BigDecimal.TEN);
+        cartItem1.setImage("Laptop Img");
+        cartItem1.setQuantity(2);
+        cartList.add(cartItem1);
 
-        CartItem product = new CartItem("Laptop", BigDecimal.TEN, false);
-        productList.add(product);
-
-        CartItem product1 = new CartItem("Laptop", BigDecimal.TEN, false);
-        productList.add(product1);
-
-        return new ResponseEntity(productList, HttpStatus.OK);
+        return new ResponseEntity(cartList, HttpStatus.OK) ;
     }
 }
