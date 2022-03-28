4aea9fe7a8784a3c0dd7c530288143e77052cc1c
Parth Deshmukh <parth.deshmukh@technogise.com>
2022-03-28 15:59:58 +0530

    caching and hateoas implemented on product controller

--- src/main/java/com/technogise/interns/shoppingcart/store/ProductController.java
+++ src/main/java/com/technogise/interns/shoppingcart/store/ProductController.java
@@ -2,11 +2,19 @@ package com.technogise.interns.shoppingcart.store;
 import com.technogise.interns.shoppingcart.dto.Product;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class ProductController {
@@ -15,26 +23,53 @@ public class ProductController {
     @ApiOperation(value = "Get all the products",
             notes = "Returns all the products from the shopping cart",
             response = Product.class)
-    public ResponseEntity<List<Product>> getAllProducts(){
-        return new ResponseEntity<>(productList, HttpStatus.OK);
+    public ResponseEntity<CollectionModel<EntityModel<Product>>>getAllProducts() {
+        List<EntityModel<Product>> entityModelList= new ArrayList<>();
+        for(Product product : productList){
+            EntityModel<Product> resource = EntityModel.of(product);
+            WebMvcLinkBuilder linkToSelf = linkTo(methodOn(this.getClass()).getProduct(product.getId()));
+            resource.add(linkToSelf.withSelfRel());
+            entityModelList.add(resource);
+        }
+        CollectionModel<EntityModel<Product>> resourceList = CollectionModel.of(entityModelList);
+        WebMvcLinkBuilder linkToSelf = linkTo(methodOn(this.getClass()).getAllProducts());
+        resourceList.add(linkToSelf.withSelfRel());
+        return ResponseEntity.ok().cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS)).body(resourceList);
     }
+
     @GetMapping("/products/{id}")
     @ApiOperation(value = "Get a single product by id",
             notes = "Returns a single product. Use the id to get the desired product.",
             response = Product.class)
-    public ResponseEntity<Product> getProduct(@ApiParam(value = "Enter Id of the product to be returned", required = true) @PathVariable(value = "id")UUID productId) {
+    public EntityModel<Product> getProduct(@ApiParam(value = "Enter Id of the product to be returned", required = true) @PathVariable(value = "id")UUID productId) {
         Product product1 = findById(productId);
-        return new ResponseEntity<>(product1, HttpStatus.OK);
+        EntityModel<Product> resource = EntityModel.of(product1);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).getAllProducts());
+        WebMvcLinkBuilder linkToSelf = linkTo(methodOn(this.getClass()).getProduct(productId));
+
+        resource.add(linkTo.withRel("all-products"));
+        resource.add(linkToSelf.withSelfRel());
+
+        return resource;
+        //return new ResponseEntity<>(product1, HttpStatus.OK);
     }
     @PostMapping(path = "/products")
     @ApiOperation(value = "Create new product",
             notes = "Creates product and add it in the shopping cart.Add the attributes of the new product. Any attribute of product if not added ,by default " +
                     "null value will be stored. Id will be auto-generated, so no need to add it.",
             response = Product.class)
-    public ResponseEntity<Product> createProduct(@ApiParam(value = "Enter new product", required = true)@RequestBody Product newProduct) {
+    public EntityModel<Product> createProduct(@ApiParam(value = "Enter new product", required = true)@RequestBody Product newProduct) {
         newProduct.setId(UUID.randomUUID());
         productList.add(newProduct);
-        return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+        EntityModel<Product> resource = EntityModel.of(newProduct);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).getAllProducts());
+        WebMvcLinkBuilder linkToGetSelf = linkTo(methodOn(this.getClass()).getProduct(newProduct.getId()));
+
+        resource.add(linkTo.withRel("all-products"));
+        resource.add(linkToGetSelf.withSelfRel());
+
+        return resource;
+        //return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
     }
     @PutMapping("/products/{id}")
     @ApiOperation(value = "Update a product by id",
