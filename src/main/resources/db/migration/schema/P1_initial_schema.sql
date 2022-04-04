46799ba9d8c6b7f9b6336906e415e6c9f5556e08
VISHAL RAJ <vishalrajput97084@gmail.com>
2022-04-04 14:55:20 +0530

    refactored DB SCHEMA

--- src/main/resources/db/migration/schema/P1_initial_schema.sql
+++ src/main/resources/db/migration/schema/P1_initial_schema.sql
@@ -17,28 +17,32 @@ create table customer(
     UNIQUE(phone_number),
     UNIQUE(email_id)
 );
-create table cartItem (
+create table cart_item (
     id UUID PRIMARY KEY,
     quantity SMALLINT NOT NULL DEFAULT 1,
-    customerID UUID REFERENCES customer(id),
-    productID UUID REFERENCES product(id),
+    customer_id UUID REFERENCES customer(id),
+    product_id UUID REFERENCES product(id),
     status boolean DEFAULT TRUE
 );
 CREATE TYPE PAYMENT_TYPE AS ENUM ('UPI', 'NET_BANKING', 'COD' , 'DEBIT_CARD');
-CREATE TYPE PAYMENT_STATUS AS ENUM ('REJECTED', 'IN_PROGRESS', 'Completed' , 'Cancelled');
+CREATE TYPE PAYMENT_STATUS AS ENUM ('REJECTED', 'IN_PROGRESS', 'COMPLETED' , 'CANCELED');
+CREATE TYPE ORDER_STATUS AS ENUM ('PLACED','PENDING','REJECTED','COMPLETED')
 create table orders (
     id UUID PRIMARY KEY,
     order_date TIMESTAMP NOT NULL,
     order_payment_type PAYMENT_TYPE NOT NULL,
     order_payment_status PAYMENT_STATUS DEFAULT 'IN_PROGRESS',
+    order_status ORDER_STATUS DEFAULT 'PLACED',
     customer_id UUID REFERENCES customer(id)
 );
+
 create table order_item(
     id UUID PRIMARY KEY,
     image VARCHAR(100),
     name VARCHAR(100) NOT NULL,
     description text,
     price NUMERIC(10,2) check(price>0) NOT NULL,
-    qty SMALLINT check(qty>0) NOT NULL,
+    quantity INT check(quantity>0) NOT NULL,
     order_id UUID REFERENCES orders(id)
 );
+
