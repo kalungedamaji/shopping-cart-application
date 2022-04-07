386b1e5898d4395edc8901df8420922923dd6bdf
VISHAL RAJ <vishalrajput97084@gmail.com>
2022-04-07 10:49:38 +0530

    added functionality for cart service

--- src/main/java/com/technogise/interns/shoppingcart/cart/service/CartService.java
+++ src/main/java/com/technogise/interns/shoppingcart/cart/service/CartService.java
@@ -1,15 +1,25 @@
 package com.technogise.interns.shoppingcart.cart.service;
 
+import com.technogise.interns.shoppingcart.cart.mapper.CartMapper;
+import com.technogise.interns.shoppingcart.cart.repository.CartRepository;
 import com.technogise.interns.shoppingcart.dto.CartItem;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
 public class CartService {
+
+        @Autowired
+        private CartRepository cartRepository;
+
         public List<CartItem> getAllCartItems() {
-                return new ArrayList<>();
+                return cartRepository.findAll()
+                       .stream()
+                       .map(CartMapper::entityToCartItemConvertor)
+                       .collect(Collectors.toList());
         }
 
 }
