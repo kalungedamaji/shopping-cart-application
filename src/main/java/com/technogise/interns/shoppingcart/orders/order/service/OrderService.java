03551eb96aa5dbd43b95833536b0acece510169d
VISHAL RAJ <vishalrajput97084@gmail.com>
2022-04-12 16:21:45 +0530

    Refactored Order service by adding get all orders

--- src/main/java/com/technogise/interns/shoppingcart/orders/order/service/OrderService.java
+++ src/main/java/com/technogise/interns/shoppingcart/orders/order/service/OrderService.java
@@ -3,9 +3,16 @@ package com.technogise.interns.shoppingcart.orders.order.service;
 import com.technogise.interns.shoppingcart.dto.Order;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrderService {
     public Order createOrder(Order order) {
         return order;
+    }
+
+    public List<Order>  getAllOrders() {
+        return new ArrayList<Order>();
     }
 }
